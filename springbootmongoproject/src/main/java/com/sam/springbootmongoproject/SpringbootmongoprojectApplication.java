f6e9f7a6f03833059c3b397019b70cdfbf53cf61
itsam2002 <51936666+itsam2002@users.noreply.github.com>
2026-01-02 15:19:56 -0500

    initial commit

---
+++ springbootmongoproject/src/main/java/com/sam/springbootmongoproject/SpringbootmongoprojectApplication.java
@@ -0,0 +1,13 @@
+package com.sam.springbootmongoproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootmongoprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootmongoprojectApplication.class, args);
+	}
+
+}
---
+++ springbootmongoproject/src/test/java/com/sam/springbootmongoproject/SpringbootmongoprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sam.springbootmongoproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootmongoprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
